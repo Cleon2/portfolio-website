8915d03690b781d5fb83207014f5e2cfd187d889
Your Name <you@example.com>
2022-11-27 18:32:20 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Intro from "./components/intro";
+import Portfolio from "./components/portfolio";
+import Skills from "./components/skills";
+import Footer from "./components/footer";
+import About from "./components/about";
+import Experience from "./components/experience";
+import NavBar from "./components/navbar";
+import { useRef } from "react";
+import Certifications from "./components/certifications";
+
+function App() {
+  const AboutRef = useRef(null);
+  const SkillsRef = useRef(null);
+  const PortfolioRef = useRef(null);
+  const ExperienceRef = useRef(null);
+
+  return (
+    <div className="App ">
+      <NavBar
+        About={AboutRef}
+        Skills={SkillsRef}
+        Portfolio={PortfolioRef}
+        Experience={ExperienceRef}
+      />
+      <Intro />
+      <About aboutRef={AboutRef} />
+      <Skills skillsRef={SkillsRef} />
+      <Portfolio portfolioRef={PortfolioRef} />
+      <Experience experienceRef={ExperienceRef} />
+      <Certifications />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,130 @@
+import React, { useReducer, useState } from "react";
+import { Turn as Hamburger } from "hamburger-react";
+import portfolio from "./data/portfolio";
+function NavBar({ About, Skills, Portfolio, Experience }) {
+  const [isOpen, setOpen] = useState(false);
+  const [aboutHover, setAboutHover] = useState(false);
+  const [skillsHover, setSkillsHover] = useState(false);
+  const [projectsHover, setProjectsHover] = useState(false);
+  const [experienceHover, setExperienceHover] = useState(false);
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      <div className=" mt-10 mx-10 md:mx-20 flex flex-row justify-between">
+        <span className="text-xl font-bold">Cleon Tan</span>
+        <span className="md:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            className="cursor-pointer"
+          />
+        </span>
+        <ul className="mx-10 text-gray-500 flex flex-row items-baseline justify-evenly w-96 hidden md:flex">
+          <li
+            className={
+              aboutHover ? "underline cursor-pointer" : "cursor-pointer"
+            }
+            onMouseEnter={() => setAboutHover(true)}
+            onMouseLeave={() => setAboutHover(false)}
+            onClick={() => scrollToSection(About)}
+          >
+            About
+          </li>
+          <li
+            className={
+              skillsHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setSkillsHover(true)}
+            onMouseLeave={() => setSkillsHover(false)}
+            onClick={() => scrollToSection(Skills)}
+          >
+            Skills
+          </li>
+          <li
+            className={
+              projectsHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setProjectsHover(true)}
+            onMouseLeave={() => setProjectsHover(false)}
+            onClick={() => scrollToSection(Portfolio)}
+          >
+            Projects
+          </li>
+          <li
+            className={
+              experienceHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setExperienceHover(true)}
+            onMouseLeave={() => setExperienceHover(false)}
+            onClick={() => scrollToSection(Experience)}
+          >
+            Experience
+          </li>
+        </ul>
+      </div>
+      {isOpen && (
+        <ul className="mx-10 md:invisible text-gray-500">
+          <li
+            className={
+              aboutHover ? "underline cursor-pointer" : "cursor-pointer"
+            }
+            onMouseEnter={() => setAboutHover(true)}
+            onMouseLeave={() => setAboutHover(false)}
+            onClick={() => scrollToSection(About)}
+          >
+            About
+          </li>
+          <li
+            className={
+              skillsHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setSkillsHover(true)}
+            onMouseLeave={() => setSkillsHover(false)}
+            onClick={() => scrollToSection(Skills)}
+          >
+            Skills
+          </li>
+          <li
+            className={
+              projectsHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setProjectsHover(true)}
+            onMouseLeave={() => setProjectsHover(false)}
+            onClick={() => scrollToSection(Portfolio)}
+          >
+            Projects
+          </li>
+          <li
+            className={
+              experienceHover
+                ? "underline mt-3 cursor-pointer"
+                : "mt-3 cursor-pointer"
+            }
+            onMouseEnter={() => setExperienceHover(true)}
+            onMouseLeave={() => setExperienceHover(false)}
+            onClick={() => scrollToSection(Experience)}
+          >
+            Experience
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default NavBar;
